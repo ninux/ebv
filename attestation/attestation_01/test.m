2b3a9ca59c0647dd118b96e68a87ed5e59237e30
Nino Ninux <nino.ninux@gmail.com>
2015-03-15 16:31:00 +0100

    began to solve some attestation requirements

---
+++ attestation/attestation_01/test.m
@@ -0,0 +1,28 @@
+% load the image
+ImageRaw = imread('diamond.bmp');
+Image = double(ImageRaw);
+
+% define parameters as struct
+Params = struct();
+Params.FilterType = 'Sobel';
+Params.Sigma = 2;
+Params.k = 0.04;
+Params.Border = 6;
+Params.Nbest = 10;
+
+% run edge detector function
+result = EdgeDetector(Image, Params);
+result = ~result;
+result = fliplr(result);
+result = flipud(result);
+
+figure(1);
+imshow(Image, []);
+
+% plot the reuslts
+figure(2);
+[rows, cols] = find(result);
+for i = 1:length(rows)
+    BBox = [cols(i)-5 rows(i)-5 10 10];
+    rectangle('Position', BBox, 'Edgecolor', [1 0 0], 'Curvature',[1,1]);
+end
---
+++ testing/foo.m
@@ -0,0 +1,8 @@
+function result = foo( args )
+%FOO Summary of this function goes here
+%   Detailed explanation goes here
+
+result = args.first * args.second;
+
+end
+
